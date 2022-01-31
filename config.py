8506eb018a36355fda618423a1a81b7893a3cb56
GeorgeM101 <georgmboya@gmail.com>
2022-01-31 18:22:50 +0300

    added source class and article class

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 
+
+
+
 API_KEY = '87e37ed0fd7d409faf6867ccc4eb61de'
--- models.py
+++ models.py
@@ -1,11 +1,31 @@
-class News:
-    '''
-    News class to define Movie Objects
-    '''
+class Sources:
+    """
+    news class to define News objects
+    """
 
-    def __init__(self, id, name, description, url, urlImage):
-        self.id = id
-        self.name = name
-        self.description = description
+    def __init__(self,id,name,author,title,description,publishedAt,Image,url):
+        self.id =id
+        self.name =name
+        self.author = author
+        self.title = title
+        self.description =description
+        self.publishedAt =publishedAt
+        self.Image = Image
         self.url = url
-        self.urlImage = urlImage
+
+
+
+class Articles:
+    """
+    article class defining article objects
+    """
+
+    def __init__(self,name,author,title,description,publishedAt,urlToImage,url):
+      
+        self.name =name
+        self.author = author
+        self.title = title
+        self.description =description
+        self.publishedAt =publishedAt
+        self.urlToImage =urlToImage
+        self.url =url        
