683fbd5fc416215d14b90a5cde982b596dcdfffc
GeorgeM101 <georgmboya@gmail.com>
2022-01-31 22:16:50 +0300

    added init file

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,17 @@
-from request import getNews
+from request import get_articles, get_sources
 from flask import render_template
-from app import app
+from . import main
 
-@app.route('/')
+@main.route('/')
 def index():
-    return render_template('index.html')
     
+    general_sources = get_sources('general')
+    business_news = get_sources('business')
+    technology_sources = get_sources('technology')
+    entertainment_sources = get_sources('entertainment')
+    science_sources = get_sources('science')
+    health_sources = get_sources('health')
+    sports_sources = get_sources('sports')
+    
+    title = 'InterNews Bringing the world to you.'
+    return render_template('index.html', title = title,general = general_sources,business=business_news,technology =technology_sources,enternainment= entertainment_sources,sports =sports_sources,science = science_sources,health= health_sources)
--- config.py
+++ config.py
@@ -1,5 +1,31 @@
 
+# from app.models import articles
+import os
+
+class Config:
+    """
+    General configaration parent class
+    """
+    NEWS_SOURCES_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    
+    EVERYTHING_SOURCE_API_URL= 'https://newsapi.org/v2/everything?q={}&apiKey={}'
+
+    SOURCE_API_KEY = os.environ.get('SOURCE_API_KEY')
+
+    
+class ProdConfig(Config):
+    """
+     Class that configures production
+    """
+
+    pass
 
 
+class DevConfig(Config):
 
-API_KEY = '87e37ed0fd7d409faf6867ccc4eb61de'
+    DEBUG =True  
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}    
